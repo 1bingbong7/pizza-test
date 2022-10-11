bbbc463bdf7b7443c806ca3c0ee2068d1ad50d59
Vince <donzalvincent@gmail.com>
2022-10-11 10:04:21 +0800

    update constant server url

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,4 +1,4 @@
-export const SERVER = "https://pizza-exam.herokuapp.com:5000"
+export const SERVER = "https://pizza-test-be.herokuapp.com"
 export const POST = "POST";
 export const GET = "GET";
 export const PUT = "PUT";
